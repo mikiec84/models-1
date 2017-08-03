0307e59a04ded6ca7c6af7859f5714c2e83be2e6
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2017-08-02 18:45:23 -0700

    ..

--- cognitive_mapping_and_planning/src/graph_nx.py
+++ cognitive_mapping_and_planning/src/graph_nx.py
@@ -2,6 +2,45 @@ import numpy as np
 from src import utils
 import networkx as nx
 
+def generate_graph_helper(valid_fn_vec=None, sc=1., n_ori=6,
+  starting_location=(0, 0, 0), vis=False, directed=True):
+  timer = utils.Timer()
+  timer.tic()
+  if directed: G = nx.DiGraph(directed=True)
+  else: G = nx.Graph()
+  G.add_node(starting_location)
+  new_nodes = G.nodes()
+  while len(new_nodes) != 0:
+    nodes_to_add = []
+    nodes_to_validate = []
+    for n in new_nodes:
+      if directed:
+        na, nv = _get_next_nodes(n, sc, n_ori)
+      else:
+        na, nv = _get_next_nodes_undirected(n, sc, n_ori)
+      nodes_to_add = nodes_to_add + na
+      if valid_fn_vec is not None:
+        nodes_to_validate = nodes_to_validate + nv
+      else:
+        node_to_add = nodes_to_add + nv
+
+    # Validate nodes.
+    vs = [_[1] for _ in nodes_to_validate]
+    valids = valid_fn_vec(vs)
+
+    for nva, valid in zip(nodes_to_validate, valids):
+      if valid:
+        nodes_to_add.append(nva)
+
+    new_nodes = []
+    for n,v,a in nodes_to_add:
+      if not G.has_node(v):
+        new_nodes.append(v)
+      G.add_edge(n, v, action=a)
+
+  timer.toc(average=True, log_at=1, log_str='src.graph_utils.generate_graph')
+  return (G)
+
 def _get_next_nodes_undirected(n, sc, n_ori):
   nodes_to_add = []
   nodes_to_validate = []
@@ -52,44 +91,5 @@ def _get_next_nodes(n, sc, n_ori):
   nodes_to_validate.append((n,v,3))
 
   return nodes_to_add, nodes_to_validate
-
-def generate_graph_helper(valid_fn_vec=None, sc=1., n_ori=6,
-  starting_location=(0, 0, 0), vis=False, directed=True):
-  timer = utils.Timer()
-  timer.tic()
-  if directed: G = nx.DiGraph(directed=True)
-  else: G = nx.Graph()
-  G.add_node(starting_location)
-  new_nodes = G.nodes()
-  while len(new_nodes) != 0:
-    nodes_to_add = []
-    nodes_to_validate = []
-    for n in new_nodes:
-      if directed:
-        na, nv = _get_next_nodes(n, sc, n_ori)
-      else:
-        na, nv = _get_next_nodes_undirected(n, sc, n_ori)
-      nodes_to_add = nodes_to_add + na
-      if valid_fn_vec is not None:
-        nodes_to_validate = nodes_to_validate + nv
-      else:
-        node_to_add = nodes_to_add + nv
-
-    # Validate nodes.
-    vs = [_[1] for _ in nodes_to_validate]
-    valids = valid_fn_vec(vs)
-
-    for nva, valid in zip(nodes_to_validate, valids):
-      if valid:
-        nodes_to_add.append(nva)
-
-    new_nodes = []
-    for n,v,a in nodes_to_add:
-      if not G.has_node(v):
-        new_nodes.append(v)
-      G.add_edge(n, v, action=a)
-
-  timer.toc(average=True, log_at=1, log_str='src.graph_utils.generate_graph')
-  return (G)
 
 
